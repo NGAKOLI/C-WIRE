2dfe54ec2fe6521f4ce8071c1deb744aa5eb1e41
NGAKOLI <ngakolifra@cy-tech.fr>
2024-12-05 10:42:32 +0000

    declaration variable station a traiter

--- c-wire.sh
+++ c-wire.sh
@@ -1,11 +1,15 @@
-echo 'Chemin du fichier:$1'
+echo 'Chemin du fichier:$0'
 
-cat  $1 |
+hvb=$2
+hva=$3
+lv=$4
+comp=$5
+indiv=$6
 
 
 if ["$2" -ne 0] && ["$3" -eq 0] ; then
-   grep "$1" 
+  cat grep "$1" 
 cat | cut 
 cat  
 if ["$2" -eq 0] && ["$3" -ne 0] ; then
-cat 'data.txt' | grep -E "^${var};.*$"
+cat 'data.txt' | grep -E "^$1;.*$"
