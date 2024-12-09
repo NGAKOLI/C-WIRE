f5bac3329bbe818885f036db122b449f16a3dfe9
NGAKOLI <ngakolifra@cy-tech.fr>
2024-12-09 10:34:57 +0000

    rajout conditions selection station

--- c-wire.sh
+++ c-wire.sh
@@ -1,14 +1,35 @@
-echo 'Chemin du fichier:$0'
+echo 'Chemin du fichier:$1'
 
-hvb=$2
-hva=$3
-lv=$4
-comp=$5
-indiv=$6
+station=$2
+consumer=$3
+powerplant=$4
 
 
-if ["$2" -ne 0] && ["$3" -eq 0] ; then
-  cat grep "$1" 
+if ["$station"!="hvb"] && ["$station"!="hva"] &&  ["$station"!="lv"]; then
+  
+  echo 'saisie incorrect:entrer hvb,hva ou lv'
+else
+if["$station" -eq "hvb"];then
+  cat 'c-wire_v00.dat' |grep -E "[0-9]+;[0-9]+;-;-;">"$1"
+  elif ["$station"-eq "hva"]; then
+  cat 'c-wire_v00.dat' | grep -E "[0-9]+;-;[0-9]+;-;">"$1"
+  elif ["$station" -eq "lv"];then
+  cat 'c-wire_v00.dat' | grep -E "[0-9]+;-;[0-9]+;[0-9]+;">"$1"
+  fi
+  fi
+
+if ["station"=="hvb"] && ["$consumer"=="all"] && ["$consumer"=="indiv"] && ["$consumer"!="comp"]; then
+ echo 'erreur'
+ fi
+ if ["station"=="hva"] && ["$consumer"=="all"] && ["$consumer"=="indiv"] && ["$consumer"!="comp"]; then
+ echo 'erreur'
+ fi
+ if ["station"=="lv"] && ["$consumer"=="all"] && ["$consumer"=="indiv"] && ["$consumer"=="comp"]; then
+ 
+ fi
+
+
+  cat  grep "$1" 
 cat | cut 
 cat  
 if ["$2" -eq 0] && ["$3" -ne 0] ; then
