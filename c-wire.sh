370e1b674409fe9d09a1c5e8226e8e1b16cea1cc
NGAKOLI <ngakolifra@cy-tech.fr>
2024-12-12 10:46:27 +0000

    c-wire.sh

--- c-wire.sh
+++ c-wire.sh
@@ -1,5 +1,5 @@
 echo 'Chemin du fichier:$1'
-
+echo 'type de station'
 station=$2
 consumer=$3
 powerplant=$4
@@ -21,16 +21,33 @@ if["$station"=="hvb"];then
 if ["station"=="hvb"] && ["$consumer"=="all"] && ["$consumer"=="indiv"] && ["$consumer"!="comp"]; then
  echo 'erreur'
  fi
+
  if ["station"=="hva"] && ["$consumer"=="all"] && ["$consumer"=="indiv"] && ["$consumer"!="comp"]; then
  echo 'erreur'
  fi
  if ["station"=="lv"] && ["$consumer"=="all"] && ["$consumer"=="indiv"] && ["$consumer"=="comp"]; then
  
  fi
+if["$powerplant" -eq 0]; then 
+ grep -E "^[^;]*;[^;]*;[^;]*;[^;]*;[^;]*;[^;]*;[^;]*$" "$DATA_FILE" > "$FILTERED_FILE"
+ else 
+ fi
 
-
+mkdir -p tmp graph
+rm -rf tmp/*
+grep 
   cat  grep "$1" 
-cat | cut 
-cat  
-if ["$2" -eq 0] && ["$3" -ne 0] ; then
-cat 'data.txt' | grep -E "^$1;.*$"
+  if["$station"=="hvb" || "$station"=="hva"]; then
+  grep -E "[0-9];[0-9]+;-;-;[0-9]+;-">tmp1.csv
+
+ grep -E "[0-9];-;[0-9];-;[0-9]+;-">tmp11.csv
+fi
+if ["$station"=="lv"];then
+if ["$consumer"=="indiv"]; then
+grep -E "[0-9];-;-;[0-9]+;[0-9]+;-">tmp1.csv
+else
+grep -E "[0-9];-;-;[0-9]+;-;[0-9];-">tmp1.csv
+fi
+fi
+  //Filtre lesdonnées
+cat "tmp1.csv" | cut -d ';' -f 2,7,8 |tr '-' '0'>fileFiltre.csv
