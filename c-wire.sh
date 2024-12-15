d57d578c8ed3172bc70f25a1f3f16b805691dcbb
An-TwoOne <169074286+An-TwoOne@users.noreply.github.com>
2024-12-15 22:26:10 +0000

    Correction shell début code

--- c-wire.sh
+++ c-wire.sh
@@ -1,38 +1,43 @@
 echo 'Chemin du fichier:$1'
 
 station=$2
-consumer=$3
-powerplant=$4
+consommateur=$3
+centrale=$4
 
 
-if ["$station"!="hvb"] && ["$station"!="hva"] &&  ["$station"!="lv"]; then
-  
-  echo 'saisie incorrect:entrer hvb,hva ou lv'
-else
-if["$station"=="hvb"];then
-  cat 'c-wire_v00.dat' |grep -E "[0-9]+;[0-9]+;-;-;">"$1"
-  elif ["$station"=="hva"]; then
-  cat 'c-wire_v00.dat' | grep -E "[0-9]+;-;[0-9]+;-;">"$1"
-  elif ["$station"=="lv"];then
-  cat 'c-wire_v00.dat' | grep -E "[0-9]+;-;[0-9]+;[0-9]+;">"$1"
-  fi
-  fi
+if [ "$station" != "hvb" ] && [ "$station" != "hva "] &&  [ " $station" != "lv" ]; then
+  echo 'Erreur : saisie incorrect. Entrer hvb, hva ou lv'     //  En plus du message d’erreur, l’aide doit aussi s’afficher en dessous, comme si l’utilisateur avait tapé l’option -h
+fi
 
-if ["station"=="hvb"] && ["$consumer"=="all"] && ["$consumer"=="indiv"] && ["$consumer"!="comp"]; then
- echo 'erreur'
- fi
- if ["station"=="hva"] && ["$consumer"=="all"] && ["$consumer"=="indiv"] && ["$consumer"!="comp"]; then
- echo 'erreur'
- fi
- if ["station"=="lv"] && ["$consumer"=="all"] && ["$consumer"=="indiv"] && ["$consumer"=="comp"]; then
+if [ "$station" = "hvb" ];then
+  cat 'c-wire_v00.dat' | grep -E "[0-9]+;[0-9]+;-;-;" > "$1"
+  elif [ "$station" = "hva"]; then
+  cat 'c-wire_v00.dat' | grep -E "[0-9]+;-;[0-9]+;-;" > "$1"
+  elif [ "$station" = "lv" ];then
+  cat 'c-wire_v00.dat' | grep -E "[0-9]+;-;[0-9]+;[0-9]+;" > "$1"
  
+  fi
+  
+
+if [ "$station" = "hvb" ] && [ "$consommateur" != "comp" ]; then
+ echo 'Erreur : seul les consommateurs entreprise sont valide pour hvb'
+fi
+
+
+if [ "$station" = "hva" ] && [ "$consommateur" != "comp" ] ; then
+ echo 'Erreur : seul les consommateurs entreprise sont valide pour hva'
+fi
+ 
+if [ "$station" = "lv" ] && { [ "$consommateur" != "all" ] && [ "$consommateur" != "indiv" ] && [ "$consommateur" != "comp" ]; }; then
+  echo "Erreur : consommateur incorrect  "
 fi
 
 echo " souhaitez-vous une centrale spécifique ? $4"  
 
-if["powerplant"==[0-5]+]
+if[ "$centrale" = [0-5]+]
 
-  cat  grep "$1" 
+  
+ cat  grep "$1" 
 cat | cut 
 cat  
 if ["$2" -eq 0] && ["$3" -ne 0] ; then
