e78f3bd8e5b972603a254268a77e07ff2e500524
NGAKOLI <ngakolifra@cy-tech.fr>
2024-12-16 08:02:44 +0000

    test git

--- c-wire.sh
+++ c-wire.sh
@@ -1,53 +1,28 @@
-echo 'Chemin du fichier:$1'
-echo 'type de station'
+echo "Chemin du fichier:$1"
 station=$2
-consumer=$3
-powerplant=$4
+consumer="$3"
+powerplant="$4"
 
 
-if ["$station"!="hvb"] && ["$station"!="hva"] &&  ["$station"!="lv"]; then
-  
-  echo 'saisie incorrect:entrer hvb,hva ou lv'
-else
-if["$station"=="hvb"];then
-  cat 'c-wire_v00.dat' |grep -E "[0-9]+;[0-9]+;-;-;">"$1"
-  elif ["$station"=="hva"]; then
-  cat 'c-wire_v00.dat' | grep -E "[0-9]+;-;[0-9]+;-;">"$1"
-  elif ["$station"=="lv"];then
-  cat 'c-wire_v00.dat' | grep -E "[0-9]+;-;[0-9]+;[0-9]+;">"$1"
-  fi
-  fi
-
-if ["station"=="hvb"] && ["$consumer"=="all"] && ["$consumer"=="indiv"] && ["$consumer"!="comp"]; then
- echo 'erreur'
- fi
-
- if ["station"=="hva"] && ["$consumer"=="all"] && ["$consumer"=="indiv"] && ["$consumer"!="comp"]; then
- echo 'erreur'
- fi
- if ["station"=="lv"] && ["$consumer"=="all"] && ["$consumer"=="indiv"] && ["$consumer"=="comp"]; then
- 
- fi
-if["$powerplant" -eq 0]; then 
- grep -E "^[^;]*;[^;]*;[^;]*;[^;]*;[^;]*;[^;]*;[^;]*$" "$DATA_FILE" > "$FILTERED_FILE"
- else 
- fi
-
-mkdir -p tmp graph
-rm -rf tmp/*
-grep 
-  cat  grep "$1" 
-  if["$station"=="hvb" || "$station"=="hva"]; then
-  grep -E "[0-9];[0-9]+;-;-;[0-9]+;-">tmp1.csv
-
- grep -E "[0-9];-;[0-9];-;[0-9]+;-">tmp11.csv
+if [ "$station" != hvb ] && [ "$station" != "hva" ] && [ "$station" != "lv" ]; then
+    echo "saisie incorrect:entrer hvb,hva ou lv"
+    exit 1;  
 fi
-if ["$station"=="lv"];then
-if ["$consumer"=="indiv"]; then
-grep -E "[0-9];-;-;[0-9]+;[0-9]+;-">tmp1.csv
-else
-grep -E "[0-9];-;-;[0-9]+;-;[0-9];-">tmp1.csv
-fi
-fi
-  //Filtre lesdonnées
-cat "tmp1.csv" | cut -d ';' -f 2,7,8 |tr '-' '0'>fileFiltre.csv
+if [ "$station" == "hvb" ] && [[ "$consumer" == "all" || "$consumer" == "indiv" || "$consumer" != "comp" ]]; then
+ echo 'saisir: comp'
+ exit 2;
+elif [ "$station" == "hva" ] && [[ "$consumer" == "all" || "$consumer" == "indiv" || "$consumer" != "comp" ]]; then
+  echo 'saisir: dcomp'
+elif [ "$station" == "lv" ] && [ "$consumer" != "all" ] && [ "$consumer" != "indiv" ] && [ "$consumer" != "comp" ]; then
+ echo "saisir:'all','indiv',comp "
+ fi
+
+
+if [ "$station" == "hvb" ]; then 
+   grep -E "[0-9]+;[0-9]+;-;-;" "$1">Hvb.csv
+elif [ "$station" == "hva" ]; then
+  cat "$1" | grep -E "[0-9]+;-;[0-9]+;-;" "$1" >Hva.csv
+elif [ "$station" == "lv" ];then
+  cat "$1" | grep -E "[0-9]+;-;[0-9]+;[0-9]+;">Lv.csv
+fi 
+
