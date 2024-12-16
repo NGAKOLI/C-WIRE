1d079c039626d848e497584b4f98cef824a43538
An-TwoOne <169074286+An-TwoOne@users.noreply.github.com>
2024-12-16 21:11:16 +0000

    ajout selection centrale spécifique

--- c-wire.sh
+++ c-wire.sh
@@ -6,7 +6,7 @@ consumer="$3"
 powerplant="$4"
 
 affiche_aide(){
-  echo " Rentrer : c-wire.sh  <type_station> <type_consommateur> "
+  echo " Rentrer : c-wire.sh  <type_station> <type_consommateur> <id_centrale> "
   echo " "
   echo " Options : " 
   echo " -h     Affiche cette aide " 
@@ -14,7 +14,7 @@ affiche_aide(){
   echo " <type_consommateur> Type de consommateur : comp, indiv, all  " 
   echo " si <type_station> = hvb alors Type de consommateur = comp " 
   echo " si <type_station> = hva alors Type de consommateur = comp "
-  echo " <id_centrale>   Identification d'une centrale en particulier" 
+  echo " <id_centrale>  Optionnel identification d'une centrale en particulier entre 1 à 5" 
 
   exit 1
 
@@ -27,17 +27,18 @@ fi
 if [ "$station" != hvb ] && [ "$station" != "hva" ] && [ "$station" != "lv" ]; then
   echo " Erreur saisie incorrect : entrer hvb,hva ou lv"
   affiche_aide
-    exit 1;  
+    exit 1 
 fi
 if [ "$station" == "hvb" ] && [[ "$consumer" == "all" || "$consumer" == "indiv" || "$consumer" != "comp" ]]; then
  echo "Erreur saisie incorrect : entrer  'comp' "
  affiche_aide
- exit 2;
+    exit 2
 elif [ "$station" == "hva" ] && [[ "$consumer" == "all" || "$consumer" == "indiv" || "$consumer" != "comp" ]]; then
   echo "  Erreur saisie incorrect : entrer  comp' "
-  
+    exit 3
 elif [ "$station" == "lv" ] && [[ "$consumer" != "all" && "$consumer" != "indiv" && "$consumer" != "comp" ]]; then
  echo "Erreur saisie incorrect : entrer 'all','indiv','comp' "
+    exit 4
 fi
 
 if [ -d "./tmp" ]; then # verifie la présence dossier tmp
@@ -63,11 +64,34 @@ elif [ "$station" == "lv" ];then
   grep -E "^[0-9]+;-;-;[0-9]+;-;[0-9]+" "/workspaces/C-WIRE/input/c-wire_v00.dat" >> ./tmp/lv_indiv.csv
 fi
 
-echo "Racine station;Identifiant;Capacity;Load" > ./tmp/filtren.csv
-grep -E "^[0-9]+;[0-9]+;-;-;" "/workspaces/C-WIRE/input/c-wire_v00.dat" | cut -d ';' -f 1,2,7,8 | tr '-' '0' >> ./tmp/filtren.csv
-grep -E "^[0-9]+;[0-9]+;[0-9]+;-;" "/workspaces/C-WIRE/input/c-wire_v00.dat" | cut -d ';' -f 2,3,7,8 | tr '-' '0'>> ./tmp/filtren.csv
-grep -E "^[0-9]+;-;[0-9]+;-;" "/workspaces/C-WIRE/input/c-wire_v00.dat" | cut -d ';' -f 3,5,7,8 | tr '-' '0' >> ./tmp/filtren.csv
-grep -E "^[0-9]+;-;-;[0-9]+;[0-9]+" "/workspaces/C-WIRE/input/c-wire_v00.dat" | cut -d ';' -f 4,5,7,8 | tr '-' '0'>> ./tmp/filtren.csv
-grep -E "^[0-9]+;-;-;[0-9]+;-;[0-9]+" "/workspaces/C-WIRE/input/c-wire_v00.dat" | cut -d ';' -f 4,6,7,8 | tr '-' '0' >> ./tmp/filtren.csv
+echo "Souhaitez-vous sélectionner une centrale spécifique ? Oui = 1"
+read choix
+
+if [[ "$choix" == "1" ]]; then
+  echo "Veuillez entrer l'identifiant de la centrale (1-5) :"
+  read id_centrale
+
+  if ! [[ "$id_centrale" =~ ^[1-5]$ ]]; then
+    echo "Erreur : identifiant incorrect"
+    affiche_aide
+    exit 5
+  fi
+  echo "Racine station;Identifiant;Capacity;Load" > ./tmp/filtren.csv
+  grep -E "^[0-9]+;[0-9]+;-;-;" "/workspaces/C-WIRE/input/c-wire_v00.dat" | grep "^$id_centrale;" | cut -d ';' -f 1,2,7,8 | tr '-' '0' >> ./tmp/filtren.csv
+  grep -E "^[0-9]+;[0-9]+;[0-9]+;-;" "/workspaces/C-WIRE/input/c-wire_v00.dat" | grep "^$id_centrale;" | cut -d ';' -f 2,3,7,8 | tr '-' '0'>> ./tmp/filtren.csv
+  grep -E "^[0-9]+;-;[0-9]+;-;" "/workspaces/C-WIRE/input/c-wire_v00.dat" | grep "^$id_centrale;" | cut -d ';' -f 3,5,7,8 | tr '-' '0' >> ./tmp/filtren.csv
+  grep -E "^[0-9]+;-;-;[0-9]+;[0-9]+" "/workspaces/C-WIRE/input/c-wire_v00.dat" | grep "^$id_centrale;" | cut -d ';' -f 4,5,7,8 | tr '-' '0'>> ./tmp/filtren.csv
+  grep -E "^[0-9]+;-;-;[0-9]+;-;[0-9]+" "/workspaces/C-WIRE/input/c-wire_v00.dat" | grep "^$id_centrale;" | cut -d ';' -f 4,6,7,8 | tr '-' '0' >> ./tmp/filtren.csv
 
 
+else 
+  
+  echo "Traitements  effectués sur toutes les centrales du fichier " 
+  echo "Racine station;Identifiant;Capacity;Load" > ./tmp/filtren.csv
+  grep -E "^[0-9]+;[0-9]+;-;-;" "/workspaces/C-WIRE/input/c-wire_v00.dat" | cut -d ';' -f 1,2,7,8 | tr '-' '0' >> ./tmp/filtren.csv
+  grep -E "^[0-9]+;[0-9]+;[0-9]+;-;" "/workspaces/C-WIRE/input/c-wire_v00.dat" | cut -d ';' -f 2,3,7,8 | tr '-' '0'>> ./tmp/filtren.csv
+  grep -E "^[0-9]+;-;[0-9]+;-;" "/workspaces/C-WIRE/input/c-wire_v00.dat" | cut -d ';' -f 3,5,7,8 | tr '-' '0' >> ./tmp/filtren.csv
+  grep -E "^[0-9]+;-;-;[0-9]+;[0-9]+" "/workspaces/C-WIRE/input/c-wire_v00.dat" | cut -d ';' -f 4,5,7,8 | tr '-' '0'>> ./tmp/filtren.csv
+  grep -E "^[0-9]+;-;-;[0-9]+;-;[0-9]+" "/workspaces/C-WIRE/input/c-wire_v00.dat" | cut -d ';' -f 4,6,7,8 | tr '-' '0' >> ./tmp/filtren.csv
+
+fi
