775ef770a98815021435d8a1b81a3c1042e1a0f0
An-TwoOne <169074286+An-TwoOne@users.noreply.github.com>
2024-12-16 22:51:32 +0000

    verification chemin du fichier

--- c-wire.sh
+++ c-wire.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-echo "Chemin du fichier:$1"
+echo "cheminfichier:$1"
+cheminfichier="$1"
 station="$2"
 consumer="$3"
 powerplant="$4"
 
+
+
 affiche_aide(){
-  echo " Rentrer : c-wire.sh  <type_station> <type_consommateur> <id_centrale> "
+  echo " Rentrer : c-wire.sh  <type_station> <type_consommateur>  "
   echo " "
   echo " Options : " 
   echo " -h     Affiche cette aide " 
@@ -14,7 +17,9 @@ affiche_aide(){
   echo " <type_consommateur> Type de consommateur : comp, indiv, all  " 
   echo " si <type_station> = hvb alors Type de consommateur = comp " 
   echo " si <type_station> = hva alors Type de consommateur = comp "
-  echo " <id_centrale>  Optionnel identification d'une centrale en particulier entre 1 à 5" 
+  echo " "
+  echo " Optionnel : si vous souhaitez identification centrale spécifique taper 1 sinon entrer une autre valeur" 
+  echo " Rentrer valeur de la centrale entre 1 à 5 "
 
   exit 1
 
@@ -24,21 +29,11 @@ if [[ "$*" == *"-h"* ]] || [[ $# -lt 3 ]]; then  # affiche option d'aide si '-h'
     affiche_aide
 fi
 
-if [ "$station" != hvb ] && [ "$station" != "hva" ] && [ "$station" != "lv" ]; then
-  echo " Erreur saisie incorrect : entrer hvb,hva ou lv"
-  affiche_aide
-    exit 1 
-fi
-if [ "$station" == "hvb" ] && [[ "$consumer" == "all" || "$consumer" == "indiv" || "$consumer" != "comp" ]]; then
- echo "Erreur saisie incorrect : entrer  'comp' "
- affiche_aide
-    exit 2
-elif [ "$station" == "hva" ] && [[ "$consumer" == "all" || "$consumer" == "indiv" || "$consumer" != "comp" ]]; then
-  echo "  Erreur saisie incorrect : entrer  comp' "
-    exit 3
-elif [ "$station" == "lv" ] && [[ "$consumer" != "all" && "$consumer" != "indiv" && "$consumer" != "comp" ]]; then
- echo "Erreur saisie incorrect : entrer 'all','indiv','comp' "
-    exit 4
+verification=0
+
+if [[ "$cheminfichier" != *.dat ]]; then
+  echo “Le fichier spécifié n’est pas un fichier .dat “
+  verification=1
 fi
 
 if [ -d "./tmp" ]; then # verifie la présence dossier tmp
@@ -48,6 +43,31 @@ else
   mkdir ./tmp          # si dossier n'existe pas le créer
 
 fi
+
+debut=$(date +%s)
+
+
+if [ "$station" != hvb ] && [ "$station" != "hva" ] && [ "$station" != "lv" ]; then
+  echo " Erreur saisie incorrect : entrer hvb,hva ou lv"
+  affiche_aide
+  exit 1 
+  
+
+fi
+if [ "$station" == "hvb" ] && [[ "$consumer" == "all" || "$consumer" == "indiv" || "$consumer" != "comp" ]]; then
+ echo "Erreur saisie incorrect : entrer  'comp' "
+ affiche_aide
+  exit 2
+  
+elif [ "$station" == "hva" ] && [[ "$consumer" == "all" || "$consumer" == "indiv" || "$consumer" != "comp" ]]; then
+  echo "  Erreur saisie incorrect : entrer  comp' "
+    exit 3
+elif [ "$station" == "lv" ] && [[ "$consumer" != "all" && "$consumer" != "indiv" && "$consumer" != "comp" ]]; then
+ echo "Erreur saisie incorrect : entrer 'all','indiv','comp' "
+  exit 4
+  
+fi
+
 
 if [ "$station" == "hvb" ]; then
     echo "Power plant;HVB-Station;HVA-Station;LV Station;Company;Individual;Capacity;Load" > ./tmp/hvb.csv
@@ -64,29 +84,33 @@ elif [ "$station" == "lv" ];then
   grep -E "^[0-9]+;-;-;[0-9]+;-;[0-9]+" "/workspaces/C-WIRE/input/c-wire_v00.dat" >> ./tmp/lv_indiv.csv
 fi
 
-echo "Souhaitez-vous sélectionner une centrale spécifique ? Oui = 1"
+fin=$(date +%s)
+
+
+echo "Souhaitez-vous sélectionner une centrale spécifique ? Oui = 1 "
 read choix
 
 if [[ "$choix" == "1" ]]; then
   echo "Veuillez entrer l'identifiant de la centrale (1-5) :"
   read id_centrale
-
+  debut2=$(date +%s)
   if ! [[ "$id_centrale" =~ ^[1-5]$ ]]; then
     echo "Erreur : identifiant incorrect"
     affiche_aide
     exit 5
+    
   fi
   echo "Racine station;Identifiant;Capacity;Load" > ./tmp/filtren.csv
-  grep -E "^[0-9]+;[0-9]+;-;-;" "/workspaces/C-WIRE/input/c-wire_v00.dat" | grep "^$id_centrale;" | cut -d ';' -f 1,2,7,8 | tr '-' '0' >> ./tmp/filtren.csv
-  grep -E "^[0-9]+;[0-9]+;[0-9]+;-;" "/workspaces/C-WIRE/input/c-wire_v00.dat" | grep "^$id_centrale;" | cut -d ';' -f 2,3,7,8 | tr '-' '0'>> ./tmp/filtren.csv
-  grep -E "^[0-9]+;-;[0-9]+;-;" "/workspaces/C-WIRE/input/c-wire_v00.dat" | grep "^$id_centrale;" | cut -d ';' -f 3,5,7,8 | tr '-' '0' >> ./tmp/filtren.csv
-  grep -E "^[0-9]+;-;-;[0-9]+;[0-9]+" "/workspaces/C-WIRE/input/c-wire_v00.dat" | grep "^$id_centrale;" | cut -d ';' -f 4,5,7,8 | tr '-' '0'>> ./tmp/filtren.csv
-  grep -E "^[0-9]+;-;-;[0-9]+;-;[0-9]+" "/workspaces/C-WIRE/input/c-wire_v00.dat" | grep "^$id_centrale;" | cut -d ';' -f 4,6,7,8 | tr '-' '0' >> ./tmp/filtren.csv
+  grep -E "^$id_centrale+;[0-9]+;-;-;" "/workspaces/C-WIRE/input/c-wire_v00.dat"  | cut -d ';' -f 1,2,7,8 | tr '-' '0' >> ./tmp/filtren.csv
+  grep -E "^$id_centrale+;[0-9]+;[0-9]+;-;" "/workspaces/C-WIRE/input/c-wire_v00.dat"  | cut -d ';' -f 2,3,7,8 | tr '-' '0'>> ./tmp/filtren.csv
+  grep -E "^$id_centrale+;-;[0-9]+;-;" "/workspaces/C-WIRE/input/c-wire_v00.dat"  | cut -d ';' -f 3,5,7,8 | tr '-' '0' >> ./tmp/filtren.csv
+  grep -E "^$id_centrale+;-;-;[0-9]+;[0-9]+" "/workspaces/C-WIRE/input/c-wire_v00.dat"  | cut -d ';' -f 4,5,7,8 | tr '-' '0'>> ./tmp/filtren.csv
+  grep -E "^$id_centrale+;-;-;[0-9]+;-;[0-9]+" "/workspaces/C-WIRE/input/c-wire_v00.dat" | cut -d ';' -f 4,6,7,8 | tr '-' '0' >> ./tmp/filtren.csv
 
 
 else 
   
-  echo "Traitements  effectués sur toutes les centrales du fichier " 
+  echo "Traitement effectué sur toutes les centrales du fichier " 
   echo "Racine station;Identifiant;Capacity;Load" > ./tmp/filtren.csv
   grep -E "^[0-9]+;[0-9]+;-;-;" "/workspaces/C-WIRE/input/c-wire_v00.dat" | cut -d ';' -f 1,2,7,8 | tr '-' '0' >> ./tmp/filtren.csv
   grep -E "^[0-9]+;[0-9]+;[0-9]+;-;" "/workspaces/C-WIRE/input/c-wire_v00.dat" | cut -d ';' -f 2,3,7,8 | tr '-' '0'>> ./tmp/filtren.csv
@@ -95,3 +119,9 @@ else
   grep -E "^[0-9]+;-;-;[0-9]+;-;[0-9]+" "/workspaces/C-WIRE/input/c-wire_v00.dat" | cut -d ';' -f 4,6,7,8 | tr '-' '0' >> ./tmp/filtren.csv
 
 fi
+
+fin2=$(date +%s)
+
+
+duree=$(( (fin - debut) + (fin2 - debut2) ))
+echo "Durée du traitement : $duree secondes"
